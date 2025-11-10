229a8acd4f1e9a99fd7ab6f67525f63a62c3a339
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-11-09 18:52:47 -0600

    chore(routes): config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,21 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'search',
+    loadComponent: () =>
+      import('./search/search.page').then((m) => m.SearchPage),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./favorites/favorites.page').then((m) => m.FavoritesPage),
+  },
+  {
+    path: 'movie-detail/:id',
+    loadComponent: () =>
+      import('./movie-detail/movie-detail.page').then((m) => m.MovieDetailPage),
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
